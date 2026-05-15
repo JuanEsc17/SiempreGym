8fb798772f630ef6ce3f602c92e01e891c0d4a5e
Malena Carrasco Dattoli <malenacdattoli@gmail.com>
2026-05-14 23:51:46 -0300

    upsi en server.js

--- backend/server.js
+++ backend/server.js
@@ -3,41 +3,28 @@ const cors = require("cors")
 
 require("./src/db")
 const authRoutes = require("./src/routes/auth")
-
-const registerRoutes = require("./src/routes/register");
-const clasesRoutes = require("./src/routes/clasesRoutes");
-const reservasRoutes = require("./src/routes/reservasRoutes");
-const usuariosRoutes = require("./src/routes/usuariosRoutes");
+const registerRoutes = require("./src/routes/register")
+const clasesRoutes = require("./src/routes/clasesRoutes")
+const reservasRoutes = require("./src/routes/reservasRoutes")
+const usuariosRoutes = require("./src/routes/usuariosRoutes")
 
 const app = express()
 
-//crear clases
-const clasesRoutes = require("./src/routes/clasesRoutes");
-const usuariosRoutes = require("./src/routes/usuariosRoutes");
-
-// middleware
 app.use(cors())
 app.use(express.json())
-//crearclases
-app.use("/api/clases", clasesRoutes);
-app.use("/api/usuarios", usuariosRoutes);
 
-//
-// ruta de prueba
 app.get("/", (req, res) => {
     res.send("Backend funcionando 🚀")
 })
 
-app.use("/api/auth", authRoutes);
-app.use("/api", registerRoutes);
-app.use("/api/clases", clasesRoutes);
-app.use("/api/reservas", reservasRoutes);
-app.use("/api/usuarios", usuariosRoutes);
+app.use("/api/auth", authRoutes)
+app.use("/api", registerRoutes)
+app.use("/api/clases", clasesRoutes)
+app.use("/api/reservas", reservasRoutes)
+app.use("/api/usuarios", usuariosRoutes)
 
-// levantar servidor
 const PORT = 3000
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`)
 })
-
